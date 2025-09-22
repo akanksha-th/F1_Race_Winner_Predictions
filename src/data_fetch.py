63f84222bc523e9aa0f14c17e7f37179e0e68c3f
akanksha-th <ak.t.kree@gmail.com>
2025-09-22 11:52:14 +0530

    added venv components

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -1,5 +1,6 @@
 import fastf1
 from fastf1 import plotting
+import pandas as pd
 
 # Enable caching
 fastf1.Cache.enable_cache('data/fastf1_cache')  # adjust path as needed
@@ -21,7 +22,17 @@ class DataFetcher:
         except Exception as e:
             print(f"Error loading session: {e}")
 
+    def get_classification(self) -> pd.DataFrame:
+        """
+        Get the classification data for the loaded session.
+        """
+        if self.session is None:
+            raise ValueError("Session not loaded. Call load_session() first.")
+        return self.session.results
+
 
 if __name__ == "__main__":
     fetcher = DataFetcher(2025, "Azerbaijan Grand Prix")
     fetcher.load_session("Qualifying")
+    quali_results = fetcher.get_classification()
+    print(quali_results)
